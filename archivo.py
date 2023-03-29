af0a845b45148ae3477886da77561fbe6b896a79
diego <diegoalejandro061292@gmail.com>
2023-03-29 12:08:21 -0500

    first commit

---
+++ archivo.py
@@ -0,0 +1,17 @@
+# x=1
+# print(x)
+# x = x +1 
+# print(x)
+# exit()
+
+# ejemplo 2
+# x = 5
+# if x < 10:
+#     print('smaller')
+
+#     if x > 20:
+#         print('bigger')
+
+# print('finish')
+
+# ejemplo3
