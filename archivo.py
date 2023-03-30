68b09d2ea15365e8acf18740e016913b28003d0e
diego <diegoalejandro061292@gmail.com>
2023-03-29 20:01:50 -0500

    avances

--- archivo.py
+++ archivo.py
@@ -1,10 +1,11 @@
+# ejemplo 1 pasos secuenciales
 # x=1
 # print(x)
 # x = x +1 
 # print(x)
 # exit()
 
-# ejemplo 2
+# ejemplo 2 pasos condicionales
 # x = 5
 # if x < 10:
 #     print('smaller')
@@ -14,4 +15,53 @@
 
 # print('finish')
 
-# ejemplo3
+# ejemplo3 pasos repetidos
+# n=5
+# while n > 0:
+#     print (n)
+
+#     n=n-1
+# print('Blastoff!')
+
+# Ejemlo 4 
+# hours= 48
+# sueldo=30.000
+# pago=hours*sueldo
+# print(pago)
+
+# # Ejemplo 5 type matters
+# eee ='hello ' + 'there'
+# # eee = eee + 1
+
+# print(type(eee))
+# print(type('hello'))
+# print(type(1))
+
+# Ejemplo 6 several types of number
+# xx=1
+# print(type(xx))
+# temp= 98.6
+# print(type(temp))
+
+# # Ejemplo 7 type conversions
+# print (float(99)+100)
+# i=42
+# print (type (i))
+# f = float(i)
+# print (f)
+# print (type (f))
+
+# integer division
+# print (10/2)
+# print (9/2)
+# print (99/100)
+# print (10.0/2.0)
+# print (99.0/100.0)
+
+# string conversions
+# sval = '123'
+# print(type(sval))
+# ival = int(sval)
+# print (type(ival))
+# print (ival+1)
+
